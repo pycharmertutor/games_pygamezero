37ccc1f8fe9db14edacf05691fa70139b5c2a634
trash_games <124088503+pycharmertutor@users.noreply.github.com>
2023-03-29 18:44:19 +0200

    Update basketball_idiot_coins (e and s images are required).py

--- basketball_idiot_coins (e and s images are required).py
+++ basketball_idiot_coins (e and s images are required).py
@@ -1,34 +1,41 @@
 #pgzero
+import time
 import random
-
+a = random.randint(100, 500)
 WIDTH = 800
 HEIGHT = 600
-
+coin_c = 99
 player = Actor('e', (400, 300))
 coins = []
 
 def update(dt):
     global player, coins
     
-    # Move the player left or right when the arrow keys are pressed
     if keyboard.left:
         player.x -= 15
     elif keyboard.right:
         player.x += 15
 
-    # Move the player up or down when the arrow keys are pressed
+
     if keyboard.up:
         player.y -= 15
     elif keyboard.down:
         player.y += 15
 
-    # Check for collisions between the player and any coins
+
     for coin in coins:
         if player.colliderect(coin):
+            global coin_c
+            print(coin_c)
+            coin_c = coin_c + 1
             coins.remove(coin)
             print('Coin collected!')
+            
+    if coin_c == 100 or coin_c >= 100:
+        coin_c = 0
+        print("game over resseting")
 
-    # Spawn a new coin randomly on the screen
+
     if len(coins) < 1:
         coin = Actor('s', (random.randint(0, WIDTH), random.randint(0, HEIGHT)))
         coins.append(coin)
@@ -37,6 +44,6 @@ def draw():
     screen.clear()
     player.draw()
     
-    # Draw all of the coins on the screen
+
     for coin in coins:
         coin.draw()
